04cbe0c9673d4b85205d4d4d6f2cceeacec64345
VAUDIN-source <info@cheminees-vaudin.fr>
2026-08-14 17:39:06 +0200

    Fix brand and category formatting for La Nordica

--- script.js
+++ script.js
@@ -77,7 +77,8 @@ const catalogues = [
   {brand:"Spartherm", category:"inserts", label:"Inserts", docs:[["Catalogue","catalogues/spartherm.pdf"]]},
 
   // CUISINIÈRES
-{marque:"La Nordica", catégorie:"cuisinières", étiquette:"Cuisinières", documents:[["Catalogue","catalogues/la-nordica.pdf"]]},
+// CUISINIÈRES
+{brand:"La Nordica", category:"cuisinieres", label:"Cuisinières", docs:[["Catalogue","catalogues/la-nordica.pdf"]]},
 
   // ACCESSOIRES DE CHEMINÉES
 {brand:"Dixneuf", category:"accessoires", label:"Accessoires de cheminées", docs:[["Catalogue","catalogues/dixneuf-accessoires-2026-2027-compresse.pdf"]]}
